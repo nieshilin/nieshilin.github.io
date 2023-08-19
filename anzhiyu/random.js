bbb78e49d9243830f235e9b5857225ad5713acbd
nieshilin <3118690873@qq.com>
2023-08-19 17:37:14 +0800

    Site updated: 2023-08-19 17:37:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/08/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/19/第一篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
